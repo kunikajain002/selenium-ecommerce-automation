26d187fea118cbca6f92590c24549328f2baf0c8
kunika jain <kunika.jain@createbytes.com>
2026-05-03 23:00:31 +0530

    Refactor of Cart and Checkout module

--- test_cart.py
+++ test_cart.py
@@ -1,25 +1,11 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from login_page import login
+from cart_page import *
 
 def test_cart(setup):
     driver = setup
-    wait = WebDriverWait(driver, 10)
 
     login(driver)
 
-    # Add product by searching the product name
-    product_name = wait.until(EC.visibility_of_element_located((By.XPATH, "//div[text() = 'Sauce Labs Backpack']")))
-    print("Product Name 1st: ", product_name.text)
-    driver.find_element(By.XPATH, "//button[@id = 'add-to-cart-sauce-labs-backpack']").click()
+    add_product(driver)
 
-    # another product by searching the product name
-    product_name_2 = wait.until(
-        EC.visibility_of_element_located((By.XPATH, "//div[text() = 'Sauce Labs Fleece Jacket']")))
-    print("Product Name 2nd: ", product_name_2.text)
-    driver.find_element(By.XPATH, "//button[@id = 'add-to-cart-sauce-labs-fleece-jacket']").click()
-
-    badge = driver.find_element(By.XPATH, "//span[@class = 'shopping_cart_badge']").text
-
-    assert badge == "2"
+    assert get_cart_count(driver) == "2"
--- test_checkout.py
+++ test_checkout.py
@@ -1,52 +1,22 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from login_page import login
+from cart_page import *
+from checkout_page import *
 
 def test_checkout(setup):
     driver = setup
-    wait = WebDriverWait(driver, 10)
 
     login(driver)
 
-    # Add product by searching the product name
-    product_name = wait.until(EC.visibility_of_element_located((By.XPATH, "//div[text() = 'Sauce Labs Backpack']")))
-    print("Product Name 1st: ", product_name.text)
-    driver.find_element(By.XPATH, "//button[@id = 'add-to-cart-sauce-labs-backpack']").click()
+    add_product(driver)
 
-    # another product by searching the product name
-    product_name_2 = wait.until(
-        EC.visibility_of_element_located((By.XPATH, "//div[text() = 'Sauce Labs Fleece Jacket']")))
-    print("Product Name 2nd: ", product_name_2.text)
-    driver.find_element(By.XPATH, "//button[@id = 'add-to-cart-sauce-labs-fleece-jacket']").click()
+    get_cart(driver)
 
-    # Click on Cart
-    wait.until(EC.visibility_of_element_located((By.XPATH, "//a[@class = 'shopping_cart_link']"))).click()
+    checkout(driver)
 
-    # checkout
-    wait.until(EC.visibility_of_element_located((By.XPATH, "//button[text()= 'Checkout']"))).click()
+    checkout_info(driver)
 
-    # Checkout information
-    wait.until(EC.visibility_of_element_located((By.XPATH, "//input[@id = 'first-name']"))).send_keys("Kunika")
-    driver.find_element(By.XPATH, "//input[@id = 'last-name']").send_keys("Jain")
-    driver.find_element(By.XPATH, "//input[@id = 'postal-code']").send_keys("122001")
-    driver.find_element(By.XPATH, "//input[@id = 'continue']").click()
+    checkout_overview(driver)
 
-    # Checkout overview
-    wait.until(EC.visibility_of_element_located((By.XPATH, "//button[@id = 'finish']"))).click()
+    validation(driver)
 
-    # Validate order completion
-    message_shown = wait.until(
-        EC.visibility_of_element_located((By.XPATH, "//h2[text() = 'Thank you for your order!']"))).text
-
-    assert message_shown == "Thank you for your order!"
-    print("Checkout successful")
-
-    # Checkout complete
-    wait.until(EC.visibility_of_element_located((By.XPATH, "//button[text() = 'Back Home']"))).click()
-
-    # logout
-    driver.find_element(By.XPATH, "//button[text() = 'Open Menu']").click()
-
-    wait.until(EC.visibility_of_element_located((By.XPATH, "//a[text() = 'Logout']"))).click()
-    print("Log Out Successful")
+    logout(driver)
